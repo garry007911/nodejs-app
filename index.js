d6c3e089daf73a59a16794963f20fd9e85e8e9af
Igor Belozerov <122268283+garry007911@users.noreply.github.com>
2024-10-28 17:25:35 +0300

    Update index6.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, world5! The API is running.');
+    res.send('Hello, world6! The API is running.');
 });
 
 app.get('/api', (req, res) => {
